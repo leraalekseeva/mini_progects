7bac2abaf7bd9413457f46bde127fd235a2ab13b
lera7aleks <lera-alekseeva-02@mail.ru>
2023-10-02 15:56:26 +0300

    возможность задавать верхнюю границу игры

--- number_guessing.py
+++ number_guessing.py
@@ -1,30 +1,42 @@
 import random
 
 
-def is_valid(num):
-    if num.isdigit() and 1 < int(num) < 100:
+def is_valid(choice, gr):
+    if choice.isdigit() and 1 <= int(choice) <= gr:
         return True
     else:
         return False
 
 
-def is_valid_str(num):
-    if num == 'да' or num == 'нет':
+def is_valid_str(choice):
+    if choice == 'да' or choice == 'нет':
         return True
     else:
         return False
 
 
-ugaday = random.randint(1, 100)
+def ran(gr):
+    return random.randint(1, gr)
+
+
 print('Добро пожаловать в числовую угадайку')
 s = ''
 n = 0
 f = True
-while f == True:
+while f:
     count_p = 0
+    num = input('введите верхнюю границу: ')
+    if num.isdigit():
+        num = int(num)
+        ugaday = ran(num)
+        print('Загадываю число от 1 до', num)
+    else:
+        print('пока я так не умею, может попробуете ввести число?) ')
+        continue
+    print()
     while n != ugaday:
         n = input('ваше предположение: ')
-        if is_valid(n):
+        if is_valid(n, num):
             n = int(n)
             count_p += 1
             if n == ugaday:
@@ -40,9 +52,9 @@ while f == True:
             continue
 
     while True:
-        qustion = input('Хотите сыграть еще? (да/нет): ')
-        if is_valid_str(qustion):
-            if qustion == 'да':
+        question = input('Хотите сыграть еще? (да/нет): ')
+        if is_valid_str(question):
+            if question == 'да':
                 ugaday = random.randint(1, 100)
                 print()
                 print('хмм, хорошо, я загадал новое число')
@@ -52,5 +64,6 @@ while f == True:
                 break
         else:
             print('Может все-таки стоило написать "да" или "нет"? Попробуем еще раз...')
+        print()
 
 print('Спасибо, что играли в числовую угадайку. Еще увидимся...')
